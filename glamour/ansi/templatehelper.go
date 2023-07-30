1761a5d12e8dc48e3d08533dbe0f6103ff76bc89
Poletansky Viktor <poletansky@gmail.com>
2023-07-30 18:57:27 +0300

    issue/8: add types to template funcs

--- glamour/ansi/templatehelper.go
+++ glamour/ansi/templatehelper.go
@@ -9,28 +9,24 @@ import (
 // TemplateFuncMap contains a few useful template helpers
 var (
 	TemplateFuncMap = template.FuncMap{
-		"Left": func(values ...interface{}) string {
-			s := values[0].(string)
-			n := values[1].(int)
+		"Left": func(s string, n int) string {
 			if n > len(s) {
 				n = len(s)
 			}
 
 			return s[:n]
 		},
-		"Matches": func(values ...interface{}) bool {
-			ok, _ := regexp.MatchString(values[1].(string), values[0].(string))
+		"Matches": func(s, pattern string) bool {
+			ok, _ := regexp.MatchString(pattern, s)
 			return ok
 		},
-		"Mid": func(values ...interface{}) string {
-			s := values[0].(string)
-			l := values[1].(int)
+		"Mid": func(s string, l int, rs ...int) string {
 			if l > len(s) {
 				l = len(s)
 			}
 
-			if len(values) > 2 {
-				r := values[2].(int)
+			if len(rs) > 0 {
+				r := rs[0]
 				if r > len(s) {
 					r = len(s)
 				}
@@ -38,17 +34,16 @@ var (
 			}
 			return s[l:]
 		},
-		"Right": func(values ...interface{}) string {
-			s := values[0].(string)
-			n := len(s) - values[1].(int)
+		"Right": func(s string, nn int) string {
+			n := len(s) - nn
 			if n < 0 {
 				n = 0
 			}
 
 			return s[n:]
 		},
-		"Last": func(values ...interface{}) string {
-			return values[0].([]string)[len(values[0].([]string))-1]
+		"Last": func(ss []string) string {
+			return ss[len(ss)-1]
 		},
 		// strings functions
 		"Compare":      strings.Compare, // 1.5+ only
