c76c12fd445a5fa1321d74ed7dbda3f61a2c0545
Poletansky Viktor <poletansky@gmail.com>
2024-07-20 15:41:36 +0300

    [master] rewrite state facility

--- state/state.go
+++ state/state.go
@@ -1,28 +1,100 @@
+// inspired by https://github.com/zlumer/tesm
 package state
 
-type Status int
-
-const (
-	StatusSuccess Status = iota
-	StatusFail
-	StatusNormal
-)
-
-// type Transitions [][]State // (source)State, Event, (target)State
-type Transitions[State, Event comparable] map[State]map[Event]State
-
-func FSM[State, Event comparable](
-	s0 State,
-	tr Transitions[State, Event],
-	events []Event,
-) (State, Status) {
-	s := s0
-	for _, e := range events {
-		to, ok := tr[s][e]
-		if !ok {
-			return *new(State), StatusFail
-		}
-		s = to
+func call[T any](f func(T), t T) {
+	if f == nil {
+		return
 	}
-	return s, StatusSuccess
+	f(t)
+}
+
+type h2[A, B any] struct {
+	A func(A)
+	B func(B)
+}
+
+type Sum2[A, B any] func(h2[A, B])
+
+type new2[A, B any] struct {
+	A func(A) Sum2[A, B]
+	B func(B) Sum2[A, B]
+}
+
+func New2[A, B any]() new2[A, B] {
+	return new2[A, B]{
+		A: func(a A) Sum2[A, B] { return func(h h2[A, B]) { call(h.A, a) } },
+		B: func(b B) Sum2[A, B] { return func(h h2[A, B]) { call(h.B, b) } },
+	}
+}
+
+type h3[A, B, C any] struct {
+	A func(A)
+	B func(B)
+	C func(C)
+}
+
+type Sum3[A, B, C any] func(h3[A, B, C])
+
+type new3[A, B, C any] struct {
+	A func(A) Sum3[A, B, C]
+	B func(B) Sum3[A, B, C]
+	C func(C) Sum3[A, B, C]
+}
+
+func New3[A, B, C any]() new3[A, B, C] {
+	return new3[A, B, C]{
+		A: func(a A) Sum3[A, B, C] { return func(h h3[A, B, C]) { call(h.A, a) } },
+		B: func(b B) Sum3[A, B, C] { return func(h h3[A, B, C]) { call(h.B, b) } },
+		C: func(c C) Sum3[A, B, C] { return func(h h3[A, B, C]) { call(h.C, c) } },
+	}
+}
+
+type h4[A, B, C, D any] struct {
+	A func(A)
+	B func(B)
+	C func(C)
+	D func(D)
+}
+
+type Sum4[A, B, C, D any] func(h4[A, B, C, D])
+
+type new4[A, B, C, D any] struct {
+	A func(A) Sum4[A, B, C, D]
+	B func(B) Sum4[A, B, C, D]
+	C func(C) Sum4[A, B, C, D]
+	D func(D) Sum4[A, B, C, D]
+}
+
+func New4[A, B, C, D any]() new4[A, B, C, D] {
+	return new4[A, B, C, D]{
+		A: func(a A) Sum4[A, B, C, D] { return func(h h4[A, B, C, D]) { call(h.A, a) } },
+		B: func(b B) Sum4[A, B, C, D] { return func(h h4[A, B, C, D]) { call(h.B, b) } },
+		C: func(c C) Sum4[A, B, C, D] { return func(h h4[A, B, C, D]) { call(h.C, c) } },
+		D: func(d D) Sum4[A, B, C, D] { return func(h h4[A, B, C, D]) { call(h.D, d) } },
+	}
+}
+
+type Hook[State, Event, Cmd any] struct {
+	state  State
+	update func(State, Event) (State, []Cmd)
+	cmds   []Cmd
+}
+
+func NewHook[State, Event, Cmd any](
+	state State,
+	update func(State, Event) (State, []Cmd),
+) Hook[State, Event, Cmd] {
+	return Hook[State, Event, Cmd]{state, update, nil}
+}
+
+func (h *Hook[S, E, C]) Send(events ...E) {
+	for _, event := range events {
+		state, cmds := h.update(h.state, event)
+		h.state = state
+		h.cmds = append(h.cmds, cmds...)
+	}
+}
+
+func (h *Hook[S, E, C]) State() S {
+	return h.state
 }
--- state/state_test.go
+++ state/state_test.go
@@ -1,37 +1,145 @@
 package state
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestHandmadeFSM(t *testing.T) {
-	type State int
-	type Event rune
-	startState := State(0)
-	stateA := State(1)
-	stateB := State(2)
-	stateC := State(3)
+func compare4[A, B, C, D comparable](a, b Sum4[A, B, C, D]) (res bool) {
+	a(h4[A, B, C, D]{
+		A: func(a A) { b(h4[A, B, C, D]{A: func(b A) { res = true }}) },
+		B: func(a B) { b(h4[A, B, C, D]{B: func(b B) { res = true }}) },
+		C: func(a C) { b(h4[A, B, C, D]{C: func(b C) { res = true }}) },
+		D: func(a D) { b(h4[A, B, C, D]{D: func(b D) { res = true }}) },
+	})
+	return
+}
 
-	transitions := Transitions[State, Event]{
-		startState: map[Event]State{'a': stateA},
-		stateA:     map[Event]State{'b': stateB},
-		stateB:     map[Event]State{'c': stateC},
+func TestHandmadeFSM(t *testing.T) {
+	type stateStart struct{}
+	type stateA struct{}
+	type stateB struct{}
+	type stateC struct{}
+	// TODO: stateFail
+	type stateh = h4[stateStart, stateA, stateB, stateC]
+	type state = Sum4[stateStart, stateA, stateB, stateC]
+
+	var newState = New4[stateStart, stateA, stateB, stateC]()
+
+	type event rune
+
+	type cmd = struct{}
+
+	update := func(state state, ev event) (s state, _ []cmd) {
+		state(stateh{
+			A: func(state stateStart) {
+				if ev == 'a' {
+					s = newState.B(stateA{})
+				}
+			},
+			B: func(state stateA) {
+				if ev == 'b' {
+					s = newState.C(stateB{})
+				}
+			},
+			C: func(stateB) {
+				if ev == 'c' {
+					s = newState.D(stateC{})
+				}
+			},
+		})
+		return
 	}
 
+	initialState := newState.A(stateStart{})
+	hook := NewHook(initialState, update)
+
 	for name, test := range map[string]struct {
-		input          string
-		expectedStatus Status
+		input         string
+		expectedState state
 	}{
-		"empty string":            {"", StatusNormal},
-		"non matching string":     {"x", StatusFail},
-		"matching string":         {"abc", StatusSuccess},
-		"partial matching string": {"ab", StatusNormal},
+		"empty string": {"", newState.A(stateStart{})},
+		// "non matching string":     {"x", StatusFail},
+		"matching string":         {"abc", newState.D(stateC{})},
+		"partial matching string": {"ab", newState.C(stateB{})},
 	} {
 		t.Run(name, func(t *testing.T) {
-			_, status := FSM(startState, transitions, []Event(test.input))
-			require.Equal(t, test.expectedStatus, status)
+			hook.Send([]event(test.input)...)
+			require.True(t, compare4(test.expectedState, hook.State()))
+		})
+	}
+}
+
+func TestLoadFSM(t *testing.T) {
+	type stateInitial struct{}
+	type stateLoading struct{ startedAt time.Time }
+	type stateLoaded struct {
+		stateLoading
+		loadedAt time.Time
+	}
+	type stateh = h3[stateInitial, stateLoading, stateLoaded]
+	type state = Sum3[stateInitial, stateLoading, stateLoaded]
+
+	var newState = New3[stateInitial, stateLoading, stateLoaded]()
+
+	type startedLoading struct{ At time.Time }
+	type finishedLoading struct{ At time.Time }
+	type eventh = h2[startedLoading, finishedLoading]
+	type event = Sum2[startedLoading, finishedLoading]
+
+	var newEvent = New2[startedLoading, finishedLoading]()
+
+	type cmdStartLoadingAnimation struct{}
+	type cmdDisplayPopup struct{ text string }
+	type cmdh = h2[cmdStartLoadingAnimation, cmdDisplayPopup]
+	type cmd = Sum2[cmdStartLoadingAnimation, cmdDisplayPopup]
+
+	var newCmd = New2[cmdStartLoadingAnimation, cmdDisplayPopup]()
+
+	update := func(state state, ev event) (s state, cmds []cmd) {
+		state(stateh{
+			A: func(state stateInitial) {
+				ev(eventh{
+					A: func(ev startedLoading) {
+						s = newState.B(stateLoading{startedAt: ev.At})
+						cmds = []cmd{newCmd.A(cmdStartLoadingAnimation{})}
+					},
+				})
+			},
+			B: func(state stateLoading) {
+				ev(eventh{
+					B: func(ev finishedLoading) {
+						s = newState.C(stateLoaded{state, ev.At})
+						cmds = []cmd{newCmd.B(cmdDisplayPopup{fmt.Sprintf(
+							`Loading finished in %d milliseconds!`,
+							ev.At.Sub(state.startedAt)/time.Millisecond,
+						)})}
+					},
+				})
+			},
+			C: func(stateLoaded) {},
+		})
+		return
+	}
+
+	initialState := newState.A(stateInitial{})
+	hook := NewHook(initialState, update)
+	fmt.Println(hook.State())
+	hook.Send(newEvent.A(startedLoading{time.Now()}))
+	fmt.Println(hook.State())
+	hook.Send(newEvent.B(finishedLoading{time.Now()}))
+	fmt.Println(hook.State())
+	for _, cmd := range hook.cmds { // handle commands
+		cmd(cmdh{
+			A: func(cmd cmdStartLoadingAnimation) {
+				fmt.Println(`loading animation started`)
+			},
+			B: func(cmd cmdDisplayPopup) {
+				fmt.Println(`displaying popup with text "${cmd.text}"`)
+			},
 		})
 	}
 }
